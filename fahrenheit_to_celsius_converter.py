2c11371ea703c378eec48f28e2c2dc7d79a3d6d2
b-chomoev <151279663+b-chomoev@users.noreply.github.com>
2023-11-18 18:32:44 -0600

    Add files via upload
    Here is the lab assignment for Module 3.

---
+++ fahrenheit_to_celsius_converter.py
@@ -0,0 +1,10 @@
+# Beksultan
+# 11/18/2023
+
+# This program converts degrees Fahrenheit to degrees Celsius
+
+user_value = int(input('Enter your Fahrenheit degrees: '))
+
+result = 5/9 * (user_value - 32)
+
+print('Congratulations !', user_value, 'Fahrenheit degree is ', result, 'in Celsius degree!')
---
+++ hello_world.py
@@ -0,0 +1,6 @@
+# Beksultan
+# 11/18/2023
+
+# This program prints 'Hello world'
+
+print('Hello World')
