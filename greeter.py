b9854ec857590a0d2c6c18716abbbb55e587c7bd
bchomoev <chomoevbeksultan@gmail.com>
2023-11-20 16:10:50 -0600

    Add Module 3, Module 4

--- greeter.py
+++
@@ -1,8 +0,0 @@
-# Beksultan
-# 11/18/2023
-
-# This program asks the user for their name and greets them with their name
-
-user_value = input('Enter your name, please')
-
-print('Hello ' + user_value + ' !')
--- hello_world.py
+++
@@ -1,6 +0,0 @@
-# Beksultan
-# 11/18/2023
-
-# This program prints 'Hello world'
-
-print('Hello World')
